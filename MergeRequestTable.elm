05475e615a3c15d6bcfeb4bcae2cdce26eb7f0fb
miciek <miciek@gmail.com>
2016-02-15 22:13:20 +0100

    refactoring and renaming

--- MergeRequestTable.elm
+++ MergeRequestTable.elm
@@ -7,6 +7,8 @@ import Http exposing (get)
 import Json.Decode as Decode exposing (Decoder, (:=))
 import Task exposing (Task)
 import Time exposing (Time, second)
+import Signal exposing (Address)
+import List exposing (map)
 
 
 -- MODEL
@@ -32,61 +34,60 @@ type alias Model =
 -- UPDATE
 
 type Action =
-  UpdatedList (List MergeRequestStats)
+  UpdateList (List MergeRequestStats)
   | Tick Time
 
-update : Action -> Model -> ( Model, Effects Action )
+update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
-    UpdatedList list ->
-      ( { model | mrStats = list }, Effects.tick Tick )
+    UpdateList list ->
+      ({ model | mrStats = list }, Effects.tick Tick)
     Tick clockTime ->
       let
         refreshInterval = 5 * second
       in
         if clockTime - model.lastRefreshTime < refreshInterval then
-          ( model, Effects.tick Tick )
+          (model, Effects.tick Tick)
         else
-          ( { model | lastRefreshTime = clockTime }, getMergeRequests )
+          ({ model | lastRefreshTime = clockTime }, mergeRequestsFetchEffect)
 
 
 -- INIT
 
 init : (Model, Effects Action)
-init = ( { mrStats = [], lastRefreshTime = 0 }, getMergeRequests)
+init = ({ mrStats = [], lastRefreshTime = 0 }, mergeRequestsFetchEffect)
 
 
 -- VIEW
 
-view : Signal.Address Action -> Model -> Html
+view : Address Action -> Model -> Html
 view address model =
-  div
-    [ layoutStyle ]
-    [ h1 [ centeredStyle ] [ text "MR✵Stats" ]
-    , table
-        [ class "hx-table"
-        , centeredStyle
-        ]
-        [ thead []
-            [ tr [] headers ]
-        , tbody []
-            (model.mrStats |> List.map (\mr -> tr [] (viewMR mr) ))
-        ]
-    ]
+  div [ layoutStyle ]
+      [ h1 [ centeredStyle ] [ text "MR✵Stats" ]
+      , table
+          [ class "hx-table"
+          , centeredStyle
+          ]
+          [ thead [] [ tr [] headers ]
+          , tbody [] (map statsView model.mrStats)
+          ]
+      ]
 
 headers : List Html
 headers =
   ["title", "upvotes", "comments", "TTM"]
-    |> List.map text
-    |> List.map (List.repeat 1)
-    |> List.map (th [])
+    |> map text
+    |> map (List.repeat 1)
+    |> map (th [])
 
-viewMR : MergeRequestStats -> List Html
-viewMR stats = [ td [] [ a [ href stats.url ] [ text stats.mergeRequest.title ] ]
-               , td [] [ text (toString stats.mergeRequest.upvotes)]
-               , td [] [ text (toString stats.commentsQty)]
-               , td [] [ text (formatTimeToMerge stats.timeToMerge) ]
-               ]
+statsView : MergeRequestStats -> Html
+statsView stats =
+  tr []
+  [ td [] [ a [ href stats.url ] [ text stats.mergeRequest.title ] ]
+  , td [] [ text (toString stats.mergeRequest.upvotes)]
+  , td [] [ text (toString stats.commentsQty)]
+  , td [] [ text (formatTimeToMerge stats.timeToMerge) ]
+  ]
 
 formatTimeToMerge : Int -> String
 formatTimeToMerge ttm =
@@ -123,12 +124,12 @@ centeredStyle =
 
 -- EFFECTS
 
-getMergeRequests : Effects Action
-getMergeRequests =
+mergeRequestsFetchEffect : Effects Action
+mergeRequestsFetchEffect =
   Http.get decodeMRs backendUrl
       |> Task.toMaybe
       |> Task.map (\s -> mrFromMaybe s)
-      |> Task.map (\mrs -> UpdatedList mrs)
+      |> Task.map (\mrs -> UpdateList mrs)
       |> Effects.task
 
 mrFromMaybe : Maybe (List MergeRequestStats) -> List MergeRequestStats
@@ -151,5 +152,3 @@ decodeMRs = Decode.list
                   ("timeToMerge" := Decode.int)
                   ("commentsQty" := Decode.int)
                   ("url" := Decode.string)
-
-
